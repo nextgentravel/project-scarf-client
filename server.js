4f3aa8483e568def3d04ec1eeec411dab84de33a
Mike Jenkins <kinetic@icloud.com>
2019-12-09 13:04:48 -0500

    added ssl forwarding

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var serveStatic = require('serve-static');
+var enforce = require('express-sslify');
+
 let app = express();
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
 app.use(serveStatic(__dirname + "/dist"));
+
 var port = process.env.PORT || 5000;
+
 app.listen(port);
+
 console.log('server started '+ port);
