ad1dfa96440525479867bd80fe1051dbae04d650
Mike Jenkins <kinetic@icloud.com>
2019-12-19 14:09:26 -0500

    Revert "vuex-easy-firestore"
    This reverts commit 324778ac7a7362b379545e650a074da43154e1af.

--- src/main.js
+++ src/main.js
@@ -4,12 +4,26 @@ import router from './router'
 import store from './store'
 import firebase from 'firebase/app';
 
+import 'firebase/auth';
 import './registerServiceWorker'
 import './quasar'
 
 Vue.config.productionTip = false
 
 let app = '';
+
+let config = {
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId,
+  measurementId: process.env.VUE_APP_measurementId,
+};
+
+firebase.initializeApp(config);
 
 firebase.auth().onAuthStateChanged(() => {
   if (!app) {
@@ -21,3 +35,4 @@ firebase.auth().onAuthStateChanged(() => {
     }).$mount('#app')
   }
 });
+
